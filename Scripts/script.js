1d88ac1523d05719e33d91f9b8f24016d8102542
Ethan K <3than.katz@gmail.com>
2018-03-23 20:53:36 -0400

    BUTTONDUCK
    OMG IT'S BUTTONDUCK

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,8 @@
 document.getElementById("movingDuck1").onclick = function() {alert("You caught me!");};
+
+$("#showDuckButton").click( () => {
+  $("#buttonDuck").show();
+});
+$("#hideDuckButton").click( () => {
+  $("#buttonDuck").hide();
+});
