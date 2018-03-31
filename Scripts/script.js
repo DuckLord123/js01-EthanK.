16b184842993b172768961fe1c564f350337660d
Ethan K <3than.katz@gmail.com>
2018-03-30 20:27:37 -0400

    Challenge 10!!

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,38 +1,44 @@
-document.getElementById("movingDuck1").onclick = function() {alert("You caught me!");};
+$("#movingDuck1").click( () => {
+  alert("You caught me!");
+});
 
-/*
 $("#showDuckButton").click( () => {
   $("#buttonDuck").show();
 });
-*/
+/*
 $("#showDuckButton").click( function(){
   $("#buttonDuck").show();
 });
-/*
+*/
 $("#hideDuckButton").click( () => {
   $("#buttonDuck").hide();
 });
-*/
+/*
 $("#hideDuckButton").click( function(){
   $("#buttonDuck").hide();
 });
+*/
+
+$("#formBSubmit").click( () => {
+  $("#favoriteBirdText").text($("#favoriteBird").val());
+});
 // ------------------------------------
 
-var iS = 7 //int Seven
+var iS = 7 //integer Seven
 var sS = "the number 7" //string Seven
-var nSS = "7" //number String Seven
+var cS = "7" //character Seven
 
 $("#variableButton").click( function(){
-  console.log(iS + iS); //a
+  console.log("int + int = " + iS + iS); //a
   console.log(iS + sS); //b
-  console.log(iS + nSS); //c
-  console.log(nSS + iS); //d
-  console.log(iS + nSS + sS); //e
+  console.log(iS + cS); //c
+  console.log(cS + iS); //d
+  console.log(iS + cS + sS); //e
   console.log(iS + iS + sS); //f
   console.log(sS + iS + iS); //g
   console.log(iS - sS); //h
-  console.log(iS - nSS); //i
-  console.log(sS - nSS); //j
-  console.log(iS == nSS); //k
-  console.log(iS === nSS); //l
+  console.log(iS - cS); //i
+  console.log(sS - cS); //j
+  console.log(iS == cS); //k
+  console.log(iS === cS); //l
 });
